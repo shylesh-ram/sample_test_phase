da214849e5c7c2984773919aafa990108cb757e1
shylesh-ram <mail.shylesh.bs@gmail.com>
2024-02-21 22:28:11 +0530

    new filed added.

--- abc.js
+++ abc.js
@@ -1,4 +1,15 @@
-const x = {
-    a: 1,
-    b: 2
+const utils = require("./function_test.js");
+
+async function run() {
+  let a = 10;
+  let b = 5;
+  const result = utils.add(a, b);
+  return result;
 }
+
+try {
+  run();
+} catch (err) {
+  console.log("ERRR occured: ", err);
+}
+
---
+++ function_test.js
@@ -0,0 +1,8 @@
+const add = (a, b) => {
+  const c = a + b;
+  return c;
+};
+
+module.exports = {
+  add,
+};
